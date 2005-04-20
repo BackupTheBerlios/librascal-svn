a1eaa49f3448cfbd3b48b6f38f7f51373ba04628
vhex <vhex@33931356-89ef-0310-8539-bda103900ae4>
2005-04-20 12:10:19 +0000

    Using CryptGenRandom() where available (nt?).

--- branches/scons/sources/common/util/random.cc
+++ branches/scons/sources/common/util/random.cc
@@ -3,15 +3,27 @@
 // $Id$
 
 #include <stdlib.h>
-#include <time.h>
+#if defined(HAVE_CryptGenRandom)
+# include <windows.h>
+# include <wincrypt.h>
+#else
+# include <time.h>
+#endif
 #include "random.h"
+
+#if defined(HAVE_CryptGenRandom)
+static HCRYPTPROV hCryptProv = NULL;
+#endif
 
 namespace funs {
 
 void faeutil_srandom(void)
 {
-#ifdef HAVE_arc4random
+#if defined(HAVE_arc4random)
 	arc4random_stir();
+#elif defined(HAVE_CryptGenRandom)
+	if (hCryptProv == NULL)
+		CryptAcquireContext (&hprov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
 #else
 	srand(time(NULL));
 #endif
@@ -22,6 +34,11 @@ unsigned int faeutil_random(unsigned int range)
 {
 #ifdef HAVE_arc4random
 	return arc4random() % range;
+#elif defined(HAVE_CryptGenRandom)
+	unsigned int rnd = 0;
+	if (hCryptProv != NULL)
+		CryptGenRandom (hCryptProv, sizeof(rnd), &rnd);
+	return rnd;
 #else
 	double tmp = (double)rand() / (double)RAND_MAX;
 	return (unsigned int)((double)range * tmp);
